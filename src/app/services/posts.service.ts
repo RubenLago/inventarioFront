2749bbc5e7d640c303f2f3033f91d5a4daa684c8
Ruben Lago <>
2021-09-10 19:50:54 +0200

    inyectado httpCliente en services

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAll() {
+    this.httpClient.get()
+  }
+
 }

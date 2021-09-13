1f27692385afba2764cdabede87e8898087c4ded
dannyatvll <86675851+dannyatvll@users.noreply.github.com>
2021-09-13 09:14:05 +0200

    Cambios en componentes

---
+++ src/app/form-creacion-productos/form-creacion-productos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-creacion-productos',
+  templateUrl: './form-creacion-productos.component.html',
+  styleUrls: ['./form-creacion-productos.component.css']
+})
+export class FormCreacionProductosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
